0d3c280b6866296b6eafedcddb39ffbef34a20a3
BetzTobias <betz.tobias30@web.de>
2024-06-26 13:12:56 +0200

    Mit hilfe von melli firebase mit intigriert

--- family/lib/main.dart
+++ family/lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:family/firebase_options.dart';
 import 'package:family/src/app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
