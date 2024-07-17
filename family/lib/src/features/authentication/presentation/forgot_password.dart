754db1a00a881d183688ee83ab999619161b95f0
David Sent <david.sent@app-akademie.com>
2024-07-17 14:48:26 +0200

    fix overflow error when extending keyboard

--- family/lib/src/features/authentication/presentation/forgot_password.dart
+++ family/lib/src/features/authentication/presentation/forgot_password.dart
@@ -46,79 +46,82 @@ class _ForgotPasswordState extends State<ForgotPassword> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromRGBO(207, 250, 255, 1),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Logo im oberen Bereich
-              Container(
-                padding: const EdgeInsets.only(bottom: 100.0),
-                child: Image.asset(
-                  'assets/Hauptlogo.png', // Pfad zum Logo-Bild
-                  width: 600, // Breite des Logos
-                  height: 200, // Höhe des Logos
-                ),
-              ),
-              Form(
-                key: _formKey,
-                child: Column(
-                  children: [
-                    const SizedBox(height: 40),
-                    TextFormField(
-                      controller: _emailController,
-                      obscureText: false,
-                      decoration: const InputDecoration(
-                        hintText: 'E-Mail Adresse',
-                      ),
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Bitte E-Mail-Adresse eingeben';
-                        }
-                        return null;
-                      },
-                    ),
-                    const SizedBox(height: 40),
-                    // Anmelde-Button
-                    ElevatedButton(
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          _resetPassword();
-                        }
-                      },
-                      style: ButtonStyle(
-                        backgroundColor: WidgetStateProperty.all<Color>(
-                          const Color(0XFFEBE216),
-                        ),
-                        foregroundColor: WidgetStateProperty.all<Color>(
-                          Colors.black, // Schriftfarbe des Buttons
-                        ),
-                      ),
-                      child: const Text('Passwort zurücksetzen'),
-                    ),
-                    const SizedBox(height: 30),
-                  ],
-                ),
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (context) => const LoginPage()),
-                  );
-                },
-                style: ButtonStyle(
-                  backgroundColor: WidgetStateProperty.all<Color>(
-                    const Color(0XFF16972A),
-                  ),
-                  foregroundColor: WidgetStateProperty.all<Color>(
-                    Colors.black, // Schriftfarbe des Buttons
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Logo im oberen Bereich
+                Container(
+                  padding: const EdgeInsets.only(bottom: 100.0),
+                  child: Image.asset(
+                    'assets/Hauptlogo.png', // Pfad zum Logo-Bild
+                    width: 600, // Breite des Logos
+                    height: 200, // Höhe des Logos
                   ),
                 ),
-                child: const Text('zurück'),
-              ),
-            ],
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 40),
+                      TextFormField(
+                        controller: _emailController,
+                        obscureText: false,
+                        decoration: const InputDecoration(
+                          hintText: 'E-Mail Adresse',
+                        ),
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Bitte E-Mail-Adresse eingeben';
+                          }
+                          return null;
+                        },
+                      ),
+                      const SizedBox(height: 40),
+                      // Anmelde-Button
+                      ElevatedButton(
+                        onPressed: () {
+                          if (_formKey.currentState!.validate()) {
+                            _resetPassword();
+                          }
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: WidgetStateProperty.all<Color>(
+                            const Color(0XFFEBE216),
+                          ),
+                          foregroundColor: WidgetStateProperty.all<Color>(
+                            Colors.black, // Schriftfarbe des Buttons
+                          ),
+                        ),
+                        child: const Text('Passwort zurücksetzen'),
+                      ),
+                      const SizedBox(height: 30),
+                    ],
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const LoginPage()),
+                    );
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all<Color>(
+                      const Color(0XFF16972A),
+                    ),
+                    foregroundColor: WidgetStateProperty.all<Color>(
+                      Colors.black, // Schriftfarbe des Buttons
+                    ),
+                  ),
+                  child: const Text('zurück'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
