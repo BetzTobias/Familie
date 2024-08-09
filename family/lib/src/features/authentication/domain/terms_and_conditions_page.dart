330fe20c5dff65343f28c9124e4eff3d06930421
BetzTobias <betz.tobias30@web.de>
2024-08-09 12:33:30 +0200

    Von Webseite auf Image umgeschrieben

--- family/lib/src/features/authentication/domain/terms_and_conditions_page.dart
+++ family/lib/src/features/authentication/domain/terms_and_conditions_page.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class TermsAndConditionsPage extends StatelessWidget {
   const TermsAndConditionsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const String url = 'Hier kommt die Webseite hin'; // Link für die AGB´s
-
     return Scaffold(
       backgroundColor: const Color.fromRGBO(207, 250, 255, 1),
       appBar: AppBar(
@@ -15,24 +12,23 @@ class TermsAndConditionsPage extends StatelessWidget {
         title: const Text('AGBs'),
       ),
       body: Center(
-        child: InkWell(
-          onTap: () async {
-            if (await canLaunch(url)) {
-              await launch(url);
-            } else {
-              // Fehlermeldung falls der Link nicht funktioniert
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(
-                    content: Text('Link konnte nicht geöffnet werden')),
-              );
-            }
-          },
-          child: const Text(
-            'Hier findest du die AGBs',
-            style: TextStyle(
-              color: Colors.blue, // Der Text wird als Link stilisiert
-              decoration: TextDecoration.underline,
-            ),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                '', // Pfad zu AGB´s Bild
+                fit: BoxFit.contain,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                ' AGBs .',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
           ),
         ),
       ),
